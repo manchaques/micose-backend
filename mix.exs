a60d8df71c7a56f2cba99a991d44b67018f2511b
Pierre PIRONIN <pironin.pierre@gmail.com>
2016-10-03 22:00:58 +0200

    Increment version.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MicoseBackend.Mixfile do
 
   def project do
     [app: :micose_backend,
-     version: "0.0.1",
+     version: "0.0.5",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
